3f0053f0de613fe3dff3ec820fd07349c62b7f29
Nicholas Boers <12831677+boersn@users.noreply.github.com>
2021-05-07 13:58:09 -0600

    Add code to test library access

---
+++ main.go
@@ -0,0 +1,18 @@
+// Copyright (c) 2021 MacEwan University. All rights reserved.
+//
+// This source code is licensed under the MIT-style license found in
+// the LICENSE file in the root directory of this source tree.
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/macewan-cs/lti/datastore/nonpersistent"
+)
+
+func main() {
+	np := nonpersistent.New()
+
+	fmt.Println("np: %v", np)
+}
